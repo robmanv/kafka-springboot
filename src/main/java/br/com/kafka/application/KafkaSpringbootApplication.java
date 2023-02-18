b5aa15395172c5d031b1f83ee1ef78d2d72bf4e5
Robson Vellasques <robson.vellasques@gmail.com>
2023-02-18 10:43:52 -0300

    minor fix

--- src/main/java/br/com/kafka/application/KafkaSpringbootApplication.java
+++ src/main/java/br/com/kafka/application/KafkaSpringbootApplication.java
@@ -3,7 +3,10 @@ package br.com.kafka.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.EnableKafka;
+
+import javax.batch.runtime.JobExecution;
 
 @EnableKafka
 @SpringBootApplication(scanBasePackages = { "br.com.kafka" })
@@ -12,6 +15,7 @@ public class KafkaSpringbootApplication {
 
 	public static void main(String[] args) {
 
+//		SpringApplication.exit(SpringApplication.run(KafkaSpringbootApplication.class, args));
 		SpringApplication.run(KafkaSpringbootApplication.class, args);
 	}
 
