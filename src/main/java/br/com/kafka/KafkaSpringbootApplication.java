9afb5cd653e6976e01003e60902a5b83f094ce92
robmanv <robson.vellasques@gmail.com>
2024-01-12 23:05:50 -0300

    Optimizing imports

--- src/main/java/br/com/kafka/KafkaSpringbootApplication.java
+++ src/main/java/br/com/kafka/KafkaSpringbootApplication.java
@@ -5,11 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
